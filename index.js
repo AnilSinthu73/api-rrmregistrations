73dfef9030ac5d70abc16bd84983868013912f9e
SINTHU ANIL <155183088+AnilSinthu73@users.noreply.github.com>
2025-02-02 23:11:50 +0530

    added vercel env

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://register.jntugv.edu.in',
   'https://rrmregistration.jntugv.edu.in'
-  "https://api-rrmregistartions.vercel.app"
+  'https://api-rrmregistartions.vercel.app'
 ];
 
 const corsOptions = {
